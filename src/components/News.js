ee4b77cd8b6b70340a3211256f82584536e2d984
Harshit Vats <hvats16@gmail.com>
2023-01-02 19:58:50 +0530

    api call'

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,42 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: this.articles,
+      };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apikey=dbe57b028aeb41e285a226a94865f7a7";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+  }
+
   render() {
     return (
       <div className="container my-3">
         <h2>NewsMonkey - Top News Headlines</h2>
+
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="text" description="test descri" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="text" description="test descri" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="text" description="test descri" />
-          </div>
+          {this.state.articles && this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 45) : ""}
+                  description={
+                    element.description ? element.description.slice(0, 88) : ""
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     );
